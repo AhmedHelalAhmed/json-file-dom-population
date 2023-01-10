9d63dd697f78fdd93a5212b72c832eee8f6e31d7
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2023-01-10 04:07:20 +0200

    Finalize logic for read json file and populate the dom

--- main.js
+++ main.js
@@ -1,4 +1,30 @@
-//Open the json file and then populate learnjs.html with the data from the json file. 
-//document.getElementsByTagName()
-//document.getElementById()
-//innerHTML();
+var MAINAPP = (function (app) {
+    var jsonObj = {};
+
+    var loadJSON = function (path) {
+        return new Promise((resolve, reject) => {
+            var xobj = new XMLHttpRequest();
+            xobj.overrideMimeType('application/json');
+            xobj.open('GET', path);
+            xobj.onreadystatechange = async function () {
+                if (xobj.readyState === 4) {
+                    app.jsonObj = JSON.parse(xobj.responseText);
+                    resolve()
+                }
+            };
+            xobj.send(null);
+        })
+
+    };
+
+    app.jsonObj = jsonObj;
+    app.loadJSON = loadJSON;
+    return app;
+})(MAINAPP || {});
+
+MAINAPP.loadJSON('data.json').then(() => {
+    document.getElementsByTagName('h2')[0].innerHTML = MAINAPP.jsonObj.heading;
+    MAINAPP.jsonObj.bullets.forEach((bullet, index) => {
+        document.getElementById('b' + (index + 1)).innerHTML = bullet;
+    });
+});
