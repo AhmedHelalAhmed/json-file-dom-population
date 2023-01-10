fee12a3cbfeb98135017d079d6eea7e91bb458dd
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2023-01-10 04:16:23 +0200

    simplify

--- main.js
+++ main.js
@@ -1,30 +1,23 @@
 var MAINAPP = (function (app) {
-    var jsonObj = {};
-
     var loadJSON = function (path) {
-        return new Promise((resolve, reject) => {
-            var xobj = new XMLHttpRequest();
-            xobj.overrideMimeType('application/json');
-            xobj.open('GET', path);
-            xobj.onreadystatechange = async function () {
-                if (xobj.readyState === 4) {
-                    app.jsonObj = JSON.parse(xobj.responseText);
-                    resolve()
-                }
-            };
-            xobj.send(null);
-        })
-
+        var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType('application/json');
+        xobj.open('GET', path);
+        xobj.onreadystatechange = async function () {
+            if (xobj.readyState === 4) {
+                populateDOM(JSON.parse(xobj.responseText));
+            }
+        };
+        xobj.send(null);
     };
 
-    app.jsonObj = jsonObj;
-    app.loadJSON = loadJSON;
+    var populateDOM = function (data) {
+        document.getElementsByTagName('h2')[0].innerHTML = data.heading;
+        data.bullets.forEach((bullet, index) => {
+            document.getElementById('b' + (index + 1)).innerHTML = bullet;
+        });
+    };
+
+    loadJSON('data.json');
     return app;
 })(MAINAPP || {});
-
-MAINAPP.loadJSON('data.json').then(() => {
-    document.getElementsByTagName('h2')[0].innerHTML = MAINAPP.jsonObj.heading;
-    MAINAPP.jsonObj.bullets.forEach((bullet, index) => {
-        document.getElementById('b' + (index + 1)).innerHTML = bullet;
-    });
-});
